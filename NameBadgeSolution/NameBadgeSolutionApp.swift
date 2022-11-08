a56bee48c5c6c2abc45f00961b3bd3ea6ebc4f19
Grace Gong <ggong@lcs.on.ca>
2022-11-08 14:19:11 -0500

    Initial Commit

---
+++ NameBadgeSolution/NameBadgeSolutionApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameBadgeSolutionApp.swift
+//  NameBadgeSolution
+//
+//  Created by Grace Gong on 2022-11-08.
+//
+
+import SwiftUI
+
+@main
+struct NameBadgeSolutionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
